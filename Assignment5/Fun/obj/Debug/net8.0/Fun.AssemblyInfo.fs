bedc48a68afac6ec81f60b1fc7894e1a61d3d89b
August Brandt <augustbrandt170@gmail.com>
2024-10-03 12:35:44 +0200

    Finito

--- Assignment5/Fun/obj/Debug/net8.0/Fun.AssemblyInfo.fs
+++ Assignment5/Fun/obj/Debug/net8.0/Fun.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Fun")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a41fa3f207826fad7de5e195300f30baaf35169")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d158b490092c0d7faa916fd5eb23bddcd947c6c")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Fun")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Fun")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
